b8d2932dc0a12bd8228ede21397eeb505628e0f2
PurpleGRStudios <133716195+PurpleGRStudios@users.noreply.github.com>
2026-09-25 00:00:22 +0200

    making progress homework handbags & purses

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import './App.css'
+import Button from './components/Button.jsx'
+
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
+  return (<>
+            <h1>Handbags & Purses</h1>
+
+        <nav>
+
+          <Button text="to the collection" disabled={false}/>
+          <Button text="shop all bags" disabled={false}/>
+          <Button text="pre-orders" disabled={true}/>
+        </nav>
+
+        <main>
+          
+        </main>
+  </>
+
+
   )
 }
 
---
+++ src/Components/Product.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Product(label, img, name, price){
+    return (
+
+    )
+}
+
+
+export default Product;
