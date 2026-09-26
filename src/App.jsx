05ab6728a57f997a58aaa253f834772f6ebb8b84
PurpleGRStudios <133716195+PurpleGRStudios@users.noreply.github.com>
2026-09-26 15:03:06 +0200

    finished handbags opdracht

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import './App.css'
 import Button from './components/Button.jsx'
+import Product from './components/Product.jsx'
+import Tile from "./Components/Tile.jsx";
+import bag_1 from './assets/bag_1.png'
+import bag_2 from './assets/bag_2.png'
+import bag_3 from './assets/bag_3.png'
+import bag_4 from './assets/bag_4.png'
+import our_story from './assets/our_story.png'
+import brand from './assets/brand.png'
 
 
 function App() {
@@ -7,15 +15,29 @@ function App() {
             <h1>Handbags & Purses</h1>
 
         <nav>
-
-          <Button text="to the collection" disabled={false}/>
-          <Button text="shop all bags" disabled={false}/>
-          <Button text="pre-orders" disabled={true}/>
+            <Button text="to the collection" disabled={false}/>
+            <Button text="shop all bags" disabled={false}/>
+            <Button text="pre-orders" disabled={true}/>
         </nav>
 
         <main>
-          
+            <Product label="Best seller" price="400" nameOfBag="The handy bag" imageAlt="bag1" image={bag_1}/>
+            <Product label="Best seller" price="250" nameOfBag="The stylish bag" imageAlt="bag2" image={bag_2}/>
+            <Product label="New collection" price="300" nameOfBag="The simple bag" imageAlt="bag3" image={bag_3}/>
+            <Product label="New collection" price="150" nameOfBag="The trendy bag" imageAlt="bag4" image={bag_4}/>
         </main>
+
+        <footer>
+            <Tile title="The brand">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid culpa dolorum enim excepturi, fuga pariatur praesentium quia sequi similique sunt.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid culpa dolorum enim excepturi, fuga pariatur praesentium quia sequi similique sunt.</p>
+            </Tile>
+            <Tile image={brand} imageDescription="The brand logo" />
+            <Tile image={our_story} imageDescription="The designers" />
+            <Tile title="Our story">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid culpa dolorum enim excepturi, fuga pariatur praesentium quia sequi similique sunt.</p>
+            </Tile>
+        </footer>
   </>
 
 
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 
-function Product(label, img, name, price){
+function Product({label, image, imageAlt, nameOfBag, price}){
     return (
-
-    )
+            <article>
+                <span>{label}</span>
+                <img src={image} alt={imageAlt}/>
+                <p>{nameOfBag}</p>
+                <h4>€{price},-</h4>
+            </article>
+    );
 }
 
 
